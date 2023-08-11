9c17f3c1a666b7a898b05df65510402266025489
Or Golan <golanor@gmail.com>
2023-08-11 13:42:09 +0300

    support for larger exps using u128

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use std::io;
 use std::io::Write;
 
+mod sieve;
+
 fn gcd(mut a: u64, b: u64) -> u64 {
     a = a.rem_euclid(b);
     if a == 0 {
@@ -13,7 +15,7 @@ fn lcm(a: u64, b: u64) -> u64 {
     (a * b) / gcd(a, b)
 }
 
-// Prompt the user for an i64.
+// Prompt the user for an u64.
 fn get_u64(prompt: &str) -> u64 {
     print!("{prompt}");
     io::stdout().flush().unwrap();
@@ -29,16 +31,17 @@ fn get_u64(prompt: &str) -> u64 {
 
 // Perform fast exponentiation.
 fn fast_exp(a: u64, b: u64) -> u64 {
-    let mut r: u64 = 1;
+    let mut r: u128 = 1;
+    let a128: u128 = a.try_into().unwrap();
     let reversed_b = b.reverse_bits();
     let bits = (0..64).map(|x| reversed_b.rotate_right(x) & 0b1);
     for bit in bits {
         r *= r;
         if bit == 1 {
-            r *= a;
+            r *= a128;
         }
     }
-    r
+    r.try_into().unwrap()
 }
 
 // Perform fast exponentiation in a modulus.
@@ -56,11 +59,12 @@ fn fast_exp_mod(a: u64, b: u64, m: u64) -> u64 {
 }
 
 fn main() {
+    sieve::sieve::main();
     loop {
         let a = get_u64("Provide the first number\n");
         let b = get_u64("Provide the second number\n");
         assert_eq!(fast_exp(a, b), a.pow(b.try_into().unwrap()));
         let m = get_u64("Provide mod\n");
-        assert_eq!(fast_exp_mod(a, b, m), a.pow(b.try_into().unwrap()) % m)
+        assert_eq!(fast_exp_mod(a, b, m), a.pow(b.try_into().unwrap()) % m);
     }
 }
